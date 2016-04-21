33a6a555811f008a64b41493925ecad28dc3f3b9
ctmakro <ctmakro@gmail.com>
2016-04-21 17:51:49 +0800

    one script left

--- run_as_dev.command
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd -- "$(dirname "$0")"
-export NODE_ENV=development
-set NODE_ENV=development
-while true; do
-  echo Starting...
-  node server.js
-done
--- run_as_dev_example.bat
+++ run_as_dev_example.bat
@@ -2,5 +2,5 @@ export NODE_ENV=development
 set NODE_ENV=development
 :loop
 @echo Starting...
-@node.exe -i server.js
+@node.exe -i server.js --connect ing.moe
 @goto :loop
