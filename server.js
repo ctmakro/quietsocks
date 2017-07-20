4c24793989329fe46cbdf7297d89340dac7dc5bc
ctmakro <ctmakro@gmail.com>
2017-07-20 14:52:25 +0800

    updated server code, no individual socks4 needed

--- server.js
+++ server.js
@@ -1,31 +1,66 @@
 var net = require('net')
 var settings = require('./settings.js');
+var socks4 = require('socks4')
 
-var workmode = 'test'
-console.log('arguments:',process.argv);
+var print = console.log
 
-var j = process.argv.indexOf('--server');
-if(j>=2){
-  workmode = 'server'
+function determine_workmode(){
+  var workmode = 'test'
+  print('arguments:',process.argv);
+
+  var j = process.argv.indexOf('--server');
+  if(j>=2){
+    workmode = 'server'
+  }
+
+  var k = process.argv.indexOf('--connect');
+  if(k>=2){
+    settings.client.server_address = process.argv[k+1];
+    workmode = 'client'
+  }
+
+  print('working in',workmode,'mode');
+  return workmode
 }
 
-var k = process.argv.indexOf('--connect');
-if(k>=2){
-  settings.client.server_address = process.argv[k+1];
-  workmode = 'client'
-}
+var workmode = determine_workmode()
 
-console.log('working in',workmode,'mode');
+var counter = 0
+function get_id(){return counter++}
+function time_string(){return new Date().toUTCString()}
 
 if(workmode=='server'||workmode=='test'){
 
+  //--socks4 server-- to simplify things
+  var s4 = socks4.createServer()
+  s4.on('connect',req=>{
+    req.accept();
+    var remote = s4.proxyRequest(req); // 'true' to pipe error to client.
+    remote.on('error',err=>{
+      print(`[socks4] socket error`)
+      print(err)
+    })
+    print(`[socks4] accept and proxy request to ${req.host}:${req.port}`)
+  })
+  s4.on('error',err=>{
+    print(`[socks4] (FATAL) server error`)
+    print(err)
+  })
+  s4.listen(settings.server.socksport,()=>{
+    print(`[socks4] proxy created on port ${settings.server.socksport}`)
+  });
+
   //--relay server-- should run on VPS side
   var relay_server = net.createServer((socket) => {
-    var socketid = Date.now().toString();
-    console.log(socketid,'trying to connect to localhost',settings.server.socksport);
+    // 'socket' is incoming
+    // 'connection' is outgoing
+
+    var socketid = get_id()
+    print(`[${socketid}] ${time_string()} incoming connection at (self):${settings.server.port}`);
 
     var connection = net.connect(settings.server.socksport,'127.0.0.1',()=>{
-      console.log(socketid, 'Established');
+      print(`[${socketid}] outgoing established`);
+
       connection.on('data',(data)=>{
         socket.write(settings.buffer_encode(data));
       })
@@ -35,25 +70,29 @@ if(workmode=='server'||workmode=='test'){
       })
 
       connection.on('end',()=>{
+        print(`[${socketid}] outgoing ended`)
         socket.end()
       })
 
       socket.on('end',()=>{
+        print(`[${socketid}] incoming ended`)
         connection.end()
       })
     })
     connection.on('error',(err)=>{
-      console.log(err);
+      print(`[${socketid}] outgoing error`)
+      print(err);
       connection.end();
       socket.end();
     })
     socket.on('error',(err)=>{
-      console.log(err);
+      print(`[${socketid}] incoming error`)
+      print(err);
       connection.end();
       socket.end();
     })
   }).listen(settings.server.port,()=>{
-    console.log('relay_server listening on',settings.server.port);
+    print(`relay_server listening on port ${settings.server.port}`);
   })
 }
 
@@ -61,11 +100,12 @@ if(workmode=='client'||workmode=='test'){
 
   //--relay server-- should run on client side
   var relay_server = net.createServer((socket) => {
-    var socketid = Date.now().toString();
-    console.log(socketid,'trying to connect to',settings.client.server_address,'port',settings.client.server_port);
+    var socketid = get_id()
+    print(`[${socketid}] ${time_string()} outgoing connection to ${settings.client.server_address}:${settings.client.server_port}`);
 
     var connection = net.connect(settings.client.server_port,settings.client.server_address,()=>{
-      console.log(socketid, 'Established');
+      print(`[${socketid}] outgoing established`);
+
       connection.on('data',(data)=>{
         socket.write(settings.buffer_decode(data));
       })
@@ -75,27 +115,32 @@ if(workmode=='client'||workmode=='test'){
       })
 
       connection.on('end',()=>{
+        print(`[${socketid}] outgoing ended`)
         socket.end()
       })
 
       socket.on('end',()=>{
+        print(`[${socketid}] incoming ended`)
         connection.end()
       })
     })
 
     connection.on('error',(err)=>{
-      console.log(err);
+      print(`[${socketid}] outgoing error`)
+      print(err);
       connection.end();
       socket.end();
     })
 
     socket.on('error',(err)=>{
-      console.log(err);
+      print(`[${socketid}] incoming error`)
+      print(err);
       connection.end();
       socket.end();
     })
   }).listen(settings.client.port,()=>{
-    console.log('relay_server(client side) listening on',settings.client.port);
+    print(`relay_server(client side) listening on port ${settings.client.port}`);
+    print(`will connect to ${settings.client.server_address}:${settings.client.server_port} on demand`)
   })
 
 }
--- settings.js
+++ settings.js
@@ -1,14 +1,17 @@
 var settings = {};
 
+var client_port = 8118; // 浏览器socks4代理指向这个端口
+var relay_port = 8338; // 客户端和服务器上这个端口要开放
+
 settings.client = {
-  port : 8118,
+  port : client_port,
   server_address : '127.0.0.1',
-  server_port : 8338,
+  server_port : relay_port,
 }
 
 settings.server = {
-  port : 8338,
-  socksport : 8228, //occupy a port...
+  port : relay_port,
+  socksport : 8228, // 服务器上这个端口不要占用
 }
 
 settings.buffer_encode = function(buffer){
@@ -26,5 +29,39 @@ settings.buffer_decode = function(buffer){
   }
   return buffer;
 }
+
+settings.request_header =
+`GET /css/images/doc-jumbotron-bg.jpg HTTP/1.1
+Host: breakwa11.github.io
+Connection: keep-alive
+Pragma: no-cache
+Cache-Control: no-cache
+User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36
+Accept: image/webp,image/apng,image/*,*/*;q=0.8
+Referer: https://breakwa11.github.io/css/project.min.css
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,ja;q=0.2,zh-TW;q=0.2`
+
+settings.response_header =
+`HTTP/1.1 200 OK
+Server: GitHub.com
+Content-Type: image/jpeg
+Last-Modified: Fri, 14 Jul 2017 16:08:16 GMT
+Access-Control-Allow-Origin: *
+Expires: Tue, 18 Jul 2017 20:59:19 GMT
+Cache-Control: max-age=600
+X-GitHub-Request-Id: A642:6626:218A00E:2DAA163:596E744E
+Content-Length: 178993
+Accept-Ranges: bytes
+Date: Thu, 20 Jul 2017 05:07:59 GMT
+Via: 1.1 varnish
+Age: 476
+Connection: keep-alive
+X-Served-By: cache-hkg6820-HKG
+X-Cache: HIT
+X-Cache-Hits: 1
+X-Timer: S1500527280.936596,VS0,VE5
+Vary: Accept-Encoding
+X-Fastly-Request-ID: 4c288fc118fdcc5ad64ab4978833d716072a348e`
 
 module.exports = settings;
